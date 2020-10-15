0eaea7c8b9113d2d5e4d517c295e98b81ed2c444
David Molnar <14124968+deviddev@users.noreply.github.com>
2020-10-15 22:05:29 +0200

    Remove test testing

--- tests/ExampleTest.php
+++ tests/ExampleTest.php
@@ -16,9 +16,6 @@ class ExampleTest extends TestCase
 
     public function testPartnerApi()
     {
-        $billingo = new Billingo();
-        $partnerApi = $billingo->api('Partner')->getApi();
-        $partnerModel = $billingo->api('Partner')->model('PartnerUpsert');
-        $this->assertInstanceOf('\Swagger\Client\Api\PartnerApi', $partnerApi);
+        // write some test
     }
 }
